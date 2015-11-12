170760cbb0ff265d56825cdeb5a45806d45a2804
LemonGG <297278806@qq.com>
2015-11-12 10:51:17 +0800

    math ..

--- README.md
+++ README.md
@@ -8,3 +8,4 @@
 
 #### 项目开发常见问题
 * [多语言开发](https://github.com/LemonGG/cocos2d-js-helper/blob/master/Languages.md "多语言开发")
+* [圆周运动代码片段](source/math_0.js)
---
+++ source/math_0.js
@@ -0,0 +1,2 @@
+_ball.x = lc.centerPoint.x + Math.cos((_ball.angle)/180*Math.PI)*this.R
+_ball.y = lc.centerPoint.y - Math.sin((_ball.angle)/180*Math.PI)*this.R
